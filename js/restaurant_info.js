40ef80c7e7046c157ea5615d0aed7ce86f9c897f
Laura Pacheco <developer@Nestors-MacBook-Pro.local>
2019-06-08 13:59:30 -0500

    Add changes requested
    * Fix aria labels and roles for some tags
    * Change weight of some headings to preserve hierarchy

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -136,7 +136,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
@@ -181,7 +181,7 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const breadcrumb = document.querySelector('#breadcrumb ul');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
